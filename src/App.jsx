0100c8b79347bdad40504e135cb00eb73c0f363f
Matheus Estevan da Silva <matheusetevan08@gmail.com>
2024-03-25 18:25:31 -0300

    primeiro commit da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import iconLuxuosos from "./assets/icon-luxury.svg"
 
 export default function App() {
   return (
-    <main className="min-h-svh bg-offwhitebg py-20 px-6">
-      <section>
+    <main className="min-h-svh bg-offwhitebg py-20 px-6 antialised lg:flex justify-center items-center">
+      <section className="lg:flex">
         <div className="bg-orange p-12 rounded-t-lg">
           <img src={iconSedans} alt="icone dos carros Sedan" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
