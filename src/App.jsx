c847daa8efc60ebfc8260c632dc0b855f6da9a99
Matheus Estevan da Silva <matheusetevan08@gmail.com>
2024-03-25 18:51:15 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Luxuosos from "./components/Luxuosos"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialised lg:flex justify-center items-center">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans />
         <Suvs />
         <Luxuosos />
-      </section>
+      </div>
     </main>
   )
 }
